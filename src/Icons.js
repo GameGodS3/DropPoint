7f342c3f8a6b4eb61094f33e3d816e244f046927
Sudev Suresh S <sudevssuresh@gmail.com>
2021-09-10 20:07:38 +0530

    Fixed Tray Icons issues for Linux and Mac

--- src/Icons.js
+++ src/Icons.js
@@ -1,18 +1,23 @@
 const path = require("path");
 
-const convertToNative = imageName => path.join(__dirname, "../static/media/", imageName);
+const convertToNative = (imageName) =>
+  path.join(__dirname, "../static/media/", imageName);
 
 const icons = {
-  droppointDefaultIcon : convertToNative("droppoint.ico"),
-  droppointMacIcon : convertToNative("droppoint.icns"),
-  audio : convertToNative("audio.png"),
-  file : convertToNative("file.png"),
-  folder : convertToNative("folder.png"),
-  image : convertToNative("image.png"),
-  multifile : convertToNative("multifile.png"),
-  text : convertToNative("text.png"),
-  video : convertToNative("video.png"),
+  droppointDefaultIcon:
+    process.platform !== "linux"
+      ? process.platform === "darwin"
+        ? convertToNative("droppoint.icns")
+        : convertToNative("droppoint.ico")
+      : convertToNative("pngLogo/droppoint-1.png"),
+  droppointMacIcon: convertToNative("droppoint.icns"),
+  audio: convertToNative("audio.png"),
+  file: convertToNative("file.png"),
+  folder: convertToNative("folder.png"),
+  image: convertToNative("image.png"),
+  multifile: convertToNative("multifile.png"),
+  text: convertToNative("text.png"),
+  video: convertToNative("video.png"),
 };
 
 module.exports = icons;
-
