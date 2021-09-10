520193421b2e7e266d04ce879ee632518d30bff1
ashishbinu <ashishbinu90@gmail.com>
2021-09-10 19:07:40 +0530

    refactor: remove redundant code.
    The icons path variable is replaced with icons object which encapsulates
    the all the icon path variables.

--- src/Icons.js
+++ src/Icons.js
@@ -1,28 +1,17 @@
 const path = require("path");
 
-let convertToNative = (imageName) => {
-  let icon = path.join(__dirname, "../static/media/", imageName);
-  return icon;
+const convertToNative = imageName => path.join(__dirname, "../static/media/", imageName);
+
+const icons = {
+  droppointDefaultIcon : convertToNative("droppoint.ico"),
+  droppointMacIcon : convertToNative("droppoint.icns"),
+  audio : convertToNative("audio.png"),
+  file : convertToNative("file.png"),
+  folder : convertToNative("folder.png"),
+  image : convertToNative("image.png"),
+  multifile : convertToNative("multifile.png"),
+  text : convertToNative("text.png"),
+  video : convertToNative("video.png"),
 };
 
-let droppointDefaultIcon = convertToNative("droppoint.ico");
-let droppointMacIcon = convertToNative("droppoint.icns");
-let audio = convertToNative("audio.png");
-let file = convertToNative("file.png");
-let folder = convertToNative("folder.png");
-let image = convertToNative("image.png");
-let multifile = convertToNative("multifile.png");
-let text = convertToNative("text.png");
-let video = convertToNative("video.png");
-
-module.exports = {
-  droppointDefaultIcon: droppointDefaultIcon,
-  droppointMacIcon: droppointMacIcon,
-  audio: audio,
-  file: file,
-  folder: folder,
-  image: image,
-  multifile: multifile,
-  text: text,
-  video: video,
-};
+module.exports = icons;
