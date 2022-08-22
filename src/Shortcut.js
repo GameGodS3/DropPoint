be05a08e6d25341f499ed35ca52d0986400d72e3
GameGodS3 <sudevssuresh@gmail.com>
2022-08-22 13:14:44 +0530

    Added: [Config] Toggle or Spawn Window on KB shortcut

--- src/Shortcut.js
+++ src/Shortcut.js
@@ -1,5 +1,7 @@
-const { globalShortcut, app } = require("electron");
+const { globalShortcut, app, webContents } = require("electron");
 const { Instance } = require("./Window");
+const Store = require("electron-store");
+const configOptions = require("./configOptions");
 
 /**
  * Sets Shift + Caps Lock as Shortcut. Change to convenience
@@ -14,13 +16,28 @@ const setShortcut = () => {
     //handle macos cmd q quitting
     globalShortcut.register("Cmd+Q", () => {
       app.exit();
-    })
+    });
   }
 
   const ret = globalShortcut.register(shortcut, () => {
-    const instance = new Instance();
-    if (instance.createNewWindow() !== null) {
-      console.log("New Window created");
+    const active_instances = webContents.getAllWebContents();
+    const config = new Store(configOptions);
+
+    console.log("Active Instances: " + active_instances);
+    if (config.get("shortcutAction") === "toggle") {
+      if (active_instances.length === 1) {
+        const instance = new Instance();
+        if (instance.createNewWindow() !== null) {
+          console.log("New Window created");
+        }
+      } else {
+        active_instances[0].send("close-signal");
+      }
+    } else {
+      const instance = new Instance();
+      if (instance.createNewWindow() !== null) {
+        console.log("New Window created");
+      }
     }
   });
 
